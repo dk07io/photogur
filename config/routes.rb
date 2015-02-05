b29630b734ba0721819de289a771ea37d77f8282
Daniel Kim <dk07io@Daniels-MacBook-Pro.local>
2015-02-05 00:55:19 -0500

    Adding feature to edit picutres

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get 'pictures' => 'pictures#index' 
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new' 
-  get 'pictures/:id' => 'pictures#show', as: 'picture' 
+  get 'pictures/:id' => 'pictures#show', as: 'picture' \
+
+  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  patch 'pictures/:id' => "pictures#update"
 
 
 
